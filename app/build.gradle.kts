f5752cc6b10902d021bacb2c4d174aead8c8f8fd
Younhwan(PC) <gihan56@ajou.ac.kr>
2023-11-30 21:43:09 +0900

    Add Sign_In, Sign_Up, Profile layout and activity

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.teammate"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.teammate"
--- app/src/main/java/com/example/teammate/MainActivity.kt
+++ app/src/main/java/com/example/teammate/MainActivity.kt
@@ -2,10 +2,28 @@ package com.example.teammate
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.content.Intent
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    fun onMainToSignUpButtonClick(view: View?){
+        val intent = Intent(this, SignUpActivity::class.java)
+        startActivity(intent)
+    }
+
+    fun onMainToSignInButtonClick(view: View?){
+        val intent = Intent(this, SignInActivity::class.java)
+        startActivity(intent)
+    }
+
+    fun onMainToProfileButtonClick(view: View?){
+        val intent = Intent(this, ProfileActivity::class.java)
+        startActivity(intent)
+    }
+
 }
