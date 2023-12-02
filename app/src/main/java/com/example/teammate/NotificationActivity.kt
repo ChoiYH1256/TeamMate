104881d7061184e4e8b24247ca8728d4dfb3ed21
Younhwan(PC) <gihan56@ajou.ac.kr>
2023-12-02 17:47:45 +0900

    Fix notification 1. notification icon 클릭시 enable/disable 설정 (나중에 DB이용하기) 2. ic_notification_active 추가

--- app/src/main/java/com/example/teammate/NotificationActivity.kt
+++ app/src/main/java/com/example/teammate/NotificationActivity.kt
@@ -4,11 +4,18 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.ImageButton
 
 class NotificationActivity : AppCompatActivity() {
+
+    private var notificationEnable = 0 // 알림 상태 (0: 비활성화, 1: 활성화)
+    private lateinit var btnNotification: ImageButton
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_notification)
+
+        btnNotification = findViewById(R.id.btn_notification)
+        updateNotificationIcon() // 초기 상태 설정
     }
 
     //뒤로가기 버튼
@@ -16,4 +23,18 @@ class NotificationActivity : AppCompatActivity() {
         finish();
     }
 
+    //notificationEnable 설정 (버튼 누르면 활성화/비활성화)
+    fun onNotificationEnableButtonClick(view: View) {
+        notificationEnable = if (notificationEnable == 0) 1 else 0
+        updateNotificationIcon()
+    }
+
+    private fun updateNotificationIcon() {
+        val iconResId = if (notificationEnable == 1) {
+            R.drawable.ic_notification_active // 활성화된 알림 아이콘
+        } else {
+            R.drawable.ic_notification // 기본 알림 아이콘
+        }
+        btnNotification.setImageResource(iconResId)
+    }
 }
