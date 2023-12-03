890fef87bb6e3138300d5f7c9b7665f8f88c551b
Younhwan(PC) <gihan56@ajou.ac.kr>
2023-12-03 15:22:02 +0900

    Fix Chat, Home, Talent, Post, Notification, AndroidManifest Add drawable, strings 1. app이 build 될수 있게 수정 2. 각 Activity에서 기능들 추가

---
+++ app/src/main/java/com/example/teammate/TalentActivity.kt
@@ -0,0 +1,32 @@
+package com.example.teammate
+
+import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+
+class TalentActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_talent)
+
+        val toolbar: Toolbar = findViewById(R.id.btn_back)
+        setSupportActionBar(toolbar)
+
+        // 툴바에 뒤로 가기 버튼 활성화
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            android.R.id.home -> {
+                // 뒤로 가기 버튼을 눌렀을 때 동작
+                onBackPressed()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+}
